3f9a0c2756c8edfac981d3d19b5874460bbf509e
Jyriz <discordketukangan123@gmail.com>
2024-12-14 16:57:07 +0700

    refactor: add enter

--- src/events/websocket/websocket.gateway.ts
+++ src/events/websocket/websocket.gateway.ts
@@ -1,10 +1,11 @@
-import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, ConnectedSocket } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, ConnectedSocket } from '@nestjs/websockets';
 
 @WebSocketGateway(3001, { cors: true })
 export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   private clients: Map<string, Socket> = new Map();
+
   handleConnection(@ConnectedSocket() socket: Socket) {
       const clientId = Array.isArray(socket.handshake.query.clientId) ? socket.handshake.query.clientId[0] : socket.handshake.query.clientId;
       console.log(`Client connected with clientId: ${clientId}`);
@@ -14,6 +15,7 @@ export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnec
       console.log('Client connected without clientId');
     }
   }
+  
   handleDisconnect(@ConnectedSocket() socket: Socket) {
     this.clients.forEach((s, clientId) => {
       if (s.id === socket.id) {
