d04eff5c0f8dab2fee633f21e8f93b16046b77c9
derispewss <derisfirmansyah177@gmail.com>
2024-12-14 16:53:59 +0700

    feat: websocket connection

--- src/events/websocket/websocket.gateway.ts
+++ src/events/websocket/websocket.gateway.ts
@@ -1,15 +1,27 @@
-import { Server } from 'socket.io';
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, ConnectedSocket } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({
-  port: 3001,
-  cors: { origin: '*' },
-})
-export class WebsocketGateway {
+@WebSocketGateway(3001, { cors: true })
+export class WebsocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
-
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  private clients: Map<string, Socket> = new Map();
+  handleConnection(@ConnectedSocket() socket: Socket) {
+      const clientId = Array.isArray(socket.handshake.query.clientId)
+        ? socket.handshake.query.clientId[0]
+        : socket.handshake.query.clientId;
+      console.log(`Client connected with clientId: ${clientId}`);
+    if (clientId) {
+      this.clients.set(clientId, socket);
+    } else {
+      console.log('Client connected without clientId');
+    }
+  }
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.clients.forEach((s, clientId) => {
+      if (s.id === socket.id) {
+        console.log(`Client with clientId: ${clientId} disconnected`);
+        this.clients.delete(clientId);
+      }
+    });
   }
 }
