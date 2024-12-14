6e3a409537b5f7e444f3a546901cb0510c4bcd04
derispewss <derisfirmansyah177@gmail.com>
2024-12-14 08:00:47 +0700

    feat: add package @nestjs/config, add console, add config env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+async function startServer() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
-bootstrap();
+
+startServer();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,9 +4,15 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     async onModuleInit() {
+        try {
         await this.$connect();
+        console.log('Database connected successfully');
+        } catch (error) {
+        console.error('Failed to connect to the database', error);
+        }
     }
     async onModuleDestroy() {
         await this.$disconnect();
+        console.log('Database disconnected');
     }
 }
