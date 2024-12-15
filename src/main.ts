9ff7b68e7d682debab2299daf39ee3304f4d9a93
derispewss <derisfirmansyah177@gmail.com>
2024-12-15 17:46:32 +0700

    feat: menambahkan configurasi swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
 
 async function startServer() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +11,23 @@ async function startServer() {
     port: configService.get<number>('PORT'),
     port_ws: configService.get<number>('PORT_WS'),
   };
+
+  // Enable CORS
   app.enableCors();
+  
+  // Swagger configuration
+  const swaggerData = JSON.parse(fs.readFileSync('settings-swagger.json', 'utf8'));
+  const config = new DocumentBuilder()
+    .setTitle(swaggerData.title)
+    .setDescription(swaggerData.description)
+    .setVersion(swaggerData.version) 
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document)
+  
+  // app listening
   await app.listen(dataPort.port);
+  console.log(`You Can Access Documentation on: http://localhost:${dataPort.port}/docs`);
   console.log(`Application is running on: http://localhost:${dataPort.port}`);
   console.log(`Websocket is running on: http://localhost:${dataPort.port_ws}`);
 }
