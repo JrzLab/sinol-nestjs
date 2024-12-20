b97cd737e459f803e9585f78340c4ef1fed7431a
Jyriz <discordketukangan123@gmail.com>
2024-12-20 08:48:51 +0700

    remove: event module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { EventsModule } from './events/events.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { WebsocketModule } from './websocket/websocket.module';
@@ -13,7 +12,6 @@ import { WebsocketModule } from './websocket/websocket.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    EventsModule,
     AuthModule,
     PrismaModule,
     WebsocketModule,
